2e485421bec8ace2d6a14730548ac8a0c86a387b
kauanascimento <165712854+kauanascimentok@users.noreply.github.com>
2025-05-29 15:22:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Botões "SAIBA MAIS"
     const saibaMaisButtons = document.querySelectorAll(".btn-info");
     saibaMaisButtons.forEach((button) => {
       button.addEventListener("click", () => {
         alert("Em breve mais informações sobre este curso!");
       });
     });
-  
-    // Botões "INSCREVER"
     const inscreverButtons = document.querySelectorAll(".btn-inscrever");
     inscreverButtons.forEach((button) => {
       button.addEventListener("click", () => {
